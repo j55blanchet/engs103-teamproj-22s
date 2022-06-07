712622ce974d2ae30686e0de093ddad295a35313
Julien Blanchet <j55blanchet@gmail.com>
2022-06-06 20:12:35 -0400

    Accounting for nonfleet ships

--- scripts/simulation/simulation.py
+++ scripts/simulation/simulation.py
@@ -1,5 +1,5 @@
 #%%
-
+import matplotlib.pyplot as plt
 from typing import List, Tuple, Union
 import numpy as np
 import pandas as pd
@@ -7,6 +7,7 @@ from dataclasses import dataclass, field
 import random
 from queue import PriorityQueue, SimpleQueue
 import numpy.random
+import math
 
 @dataclass
 class Event:
@@ -57,12 +58,13 @@ class Terminal:
     def can_fit_ship(self, ship: Ship):
         return self.curent_ship is None and self.bearth_length >= ship.length
     
-    def dock_ship(self, t: float, ship: Ship, rng: np.random.Generator):
+    def dock_ship(self, t: float, ship: Ship, rng: np.random.Generator, dock_time_mean: float, dock_time_sd: float):
         self.curent_ship = ship
 
         expected_cargo_time = ship.capacity * self.days_per_teu
         randomized_cargo_time = rng.exponential(expected_cargo_time)
-        self.completion_date = t + randomized_cargo_time
+        randomized_dock_time = rng.normal(dock_time_mean, dock_time_sd)
+        self.completion_date = t + randomized_cargo_time + randomized_dock_time
 
     @property
     def next_event(self):
@@ -94,10 +96,19 @@ class Simulation:
         nonfleet_length_sd: float = 100.0,
         nonfleet_teu_per_length_mean: float = 5.5,
         nonfleet_teu_per_length_sd: float = 0.5,
+        nonfleet_cargo_utilization_normal: float = 0.3,
+        nonfleet_cargo_utilization_sd: float = 0.1,
+        docking_time_mean: float = 3.5 / 24.0, # 3.5 hrs
+
+        docking_time_sd: float = 0.5 / 24.0, # 30 min
     ):
         ship_data = pd.read_csv(ship_data_filepath)
         terminal_data = pd.read_csv(terminal_data_filepath)
         port_data = pd.read_csv(port_data_filepath)
+
+        self.event_counters = {}
+
+        self.queue_length_over_time = []
 
         self.nonfleet_ship_counter = 0
         self.nonfleet_arrival_rate = nonfleet_arrival_rate
@@ -105,17 +116,19 @@ class Simulation:
         self.nonfleet_length_sd = nonfleet_length_sd
         self.nonfleet_teu_per_length_mean = nonfleet_teu_per_length_mean
         self.nonfleet_teu_per_length_sd = nonfleet_teu_per_length_sd
+        self.nonfleet_cargo_utilization_normal = nonfleet_cargo_utilization_normal
+        self.nonfleet_cargo_utilization_sd = nonfleet_cargo_utilization_sd
+
+        self.docking_time_mean = docking_time_mean
+        self.docking_time_sd = docking_time_sd
 
         self.rng = rng
-
-        self.last_event_t = 0
-
         self.terminals = [
             Terminal(
                 name=row["Name"],
                 bearth_length=row["Berth"],
                 # container_yard_area=row["Container Yard Area"],
-                cranes=1, #row["Total_Crane"],
+                cranes=row["Total_Crane"],
                 id=-1,
             )
             for i, row in 
@@ -125,14 +138,12 @@ class Simulation:
         for i in range(len(self.terminals)):
             self.terminals[i].id = i
 
-        self.terminals = [
-            self.terminals[0]
-        ]
-
         self._ships_in_transit = PriorityQueue()
         self._count_ships_in_transit = 0
 
         self.next_nonfleet_ship: Union[None, Tuple[float, Ship]] = None
+        self.simulate_next_nonfleet_ship(cur_time=0.0)
+        self.last_event = 0
 
         for i, row in ship_data.iterrows():
 
@@ -183,16 +194,6 @@ class Simulation:
         self._ships_in_transit.put((eta, ship))
         self._count_ships_in_transit += 1
 
-    def print_status(self):
-
-        print(f"{len(self.ships_in_queue)} ships in queue")
-        print(f"{self._count_ships_in_transit} ships in transit")
-        print("Terminal Status:")
-        for terminal in self.terminals:
-            term_status = "empty" if terminal.curent_ship is None else terminal.curent_ship.name
-            print(f"\t{terminal.name}: {term_status}")
-        print(f"Next event: {self.next_event}")
-    
     @property
     def next_event(self):
         next_event = self.next_ship_arrival_event
@@ -205,17 +206,44 @@ class Simulation:
         return next_event
 
     def update(self):
+        event = self.next_event
         if isinstance(self.next_event, ShipArrivalEvent):
+            if self.next_event.is_fleet_ship:
+                self.event_counters["fleet_ship_arrives"] = self.event_counters.get("fleet_ship_arrives", 0) + 1
+            else:
+                self.event_counters["nonfleet_ship_arrives"] = self.event_counters.get("nonfleet_ship_arrives", 0) + 1
             self.handle_ship_arrival(self.next_event)
+            
         elif isinstance(self.next_event, CargoProcessedEvent):
             self.handle_cargo_processed(self.next_event)
+            self.event_counters["cargo_processed"] = self.event_counters.get("cargo_processed", 0) + 1
+        self.last_event = event
+
+        self.queue_length_over_time.append(
+            (event.timestamp,len(self.ships_in_queue))
+        )
 
     def simulate_next_nonfleet_ship(self, cur_time: float):
         self.nonfleet_ship_counter += 1
         
-        time_to_arrival = self.rng.exponential
+        PERCENT_CARGO_THAT_IS_IN_OUR_FLEET = 0.7
+        percent_cargo_on_nonfleet = 1 - PERCENT_CARGO_THAT_IS_IN_OUR_FLEET
+        time_to_arrival_adjustment_ratio = 1 / percent_cargo_on_nonfleet
 
-        pass
+        time_to_arrival = time_to_arrival_adjustment_ratio * self.rng.exponential(1 / self.nonfleet_arrival_rate)
+        ship_length = self.rng.normal(self.nonfleet_length_mean, self.nonfleet_length_sd)
+        ship_teu = ship_length * self.rng.normal(self.nonfleet_teu_per_length_mean, self.nonfleet_teu_per_length_sd)
+        cargo_utilization = max(0.05, self.rng.normal(self.nonfleet_cargo_utilization_normal, self.nonfleet_cargo_utilization_sd))
+
+        ship = Ship(
+            name = f"Non-fleet ship {self.nonfleet_ship_counter}",
+            capacity = ship_teu,
+            roundtrip_mean = math.inf,
+            roundtrip_sd=0,
+            longbeach_cargo = cargo_utilization * ship_teu,
+            is_fleet_ship = False
+        )
+        self.next_nonfleet_ship = (cur_time + time_to_arrival, ship)
 
     def handle_ship_arrival(self, event: ShipArrivalEvent):
 
@@ -226,12 +254,12 @@ class Simulation:
         else:
             # nonfleet ship
             _, ship = self.next_nonfleet_ship
-            self.simulate_next_nonfleet_ship()
+            self.simulate_next_nonfleet_ship(event.timestamp)
 
         # Case 1 - if space at a terminal, assign ship directly to terminal
         for terminal in self.terminals:
             if terminal.can_fit_ship(ship):
-                terminal.dock_ship(event.timestamp, ship, self.rng)
+                terminal.dock_ship(event.timestamp, ship, self.rng, self.docking_time_mean, self.docking_time_sd)
                 return
 
         # Case 2 - no space at a terminal, enqueue ship
@@ -250,9 +278,32 @@ class Simulation:
         for i in range(len(self.ships_in_queue)):
             ship = self.ships_in_queue[i]
             if terminal.can_fit_ship(ship):
-                terminal.dock_ship(event.timestamp, ship, self.rng)
+                terminal.dock_ship(event.timestamp, ship, self.rng, self.docking_time_mean, self.docking_time_sd)
                 self.ships_in_queue.pop(i)
                 return
+
+    def print_status(self):
+
+        print(f"{len(self.ships_in_queue)} ships in queue")
+        print(f"{self._count_ships_in_transit} ships in transit")
+        print("Terminal Status:")
+        for terminal in self.terminals:
+            term_status = "empty" if terminal.curent_ship is None else terminal.curent_ship.name
+            print(f"\t{terminal.name}: {term_status}", end='')
+            finishes = terminal.completion_date
+            print(f'\t{finishes=}')
+        print(f"Last event: {self.last_event}")
+        print(f"Next event: {self.next_event}")
+        if len(self.event_counters) > 0:
+            print("Events:")
+        for key, value in self.event_counters.items():
+            print(f"\t{key}: {value}")
+
+    def print_queue_length_over_time(self):
+        plt.plot([d[0] for d in self.queue_length_over_time], [d[1] for d in self.queue_length_over_time])
+        plt.xlabel('Days')
+        plt.ylabel('Queue length')
+        plt.show()
 
     def run(self, event_count: int):
         print(f"Running simulation for {event_count} events")
@@ -276,9 +327,17 @@ def main():
     print()
     simulation.print_status()
 
-    simulation.run(1000)
-
+    # print()
+    # simulation.run(1000)
+    # print()
+    # simulation.print_status()
+    
+    print()
+    simulation.run(10000)
+    print()
     simulation.print_status()
+
+    simulation.print_queue_length_over_time()
 
 
 
